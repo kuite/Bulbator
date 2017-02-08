3a0517f7e65926fd00b1fca4fc0c82d3827b502a
kuite <piomost@gmail.com>
2017-02-09 00:21:24 +0100

    sync

--- OnetEmailCreator.py
+++ OnetEmailCreator.py
@@ -1,36 +1,67 @@
+import selenium.webdriver as webdriver
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.support.ui import Select
+
+
 class OnetEmailCreator:
-    inputs_selectors = {
-        'full name': '#f_nameSurname',
-        'login': '#login_user',
-        'city_postalcode': '#f_postcodePlace',
-        'password': '#f_password',
-        'repeat_password': '#f_confirmPassword',
-        'alternative_contact': '#f_phonesEmails'
+    login_selector = 'login_user'
+
+    inputs = {
+        'fullname': 'f_nameSurname',
+        'city_postalcode': 'f_postcodePlace',
+        'password': 'f_password',
+        'repeat_password': 'f_confirmPassword',
+        # 'alternative_contact': 'f_phonesEmails'
     }
     checkboxes_selectors = {
         'gender_woman': '#f_gender_K',
         'gender_man': '#f_gender_M',
-        'agreements': '#f_confirm',
-        'captcha': '#recaptcha-anchor > div.recaptcha-checkbox-checkmark'
+        'agreements': 'f_confirm',
+        'captcha': 'recaptcha-anchor > div.recaptcha-checkbox-checkmark'
     }
     dropdowns_selectors = {
-        'birthdate_day': '#f_birthDate_day',
-        'birthdate_month': '#registerForm > div > div.fieldsetWrap > div:nth-child(1) > fieldset > div > div > ul > '
-                           'li:nth-child(3) > select.monthSelect',
-        'birthdate_year': '#registerForm > div > div.fieldsetWrap > div:nth-child(1) > fieldset > div > div > ul > '
+        'birthdate_day': 'f_birthDate_day',
+        'birthdate_month': '//*[@id="registerForm"]/div/div[3]/div[1]/fieldset/div/div/ul/li[3]/select[2]',
+        'birthdate_year': 'registerForm > div > div.fieldsetWrap > div:nth-child(1) > fieldset > div > div > ul > '
                           'li:nth-child(3) > select.yearSelect',
-        'country': '#f_country'
+        'country': 'f_country'
     }
+    buttons = {
+        1: '//*[@id="registerForm"]/div/div[3]/div[2]/fieldset/div/div/ul/li/div/div[2]/span/input',
+        2: '//*[@id="registerForm"]/div/div[4]/span/input'
+    }
+
+    check_login_button = '//*[@id="registerForm"]/div/div[3]/div[1]/fieldset/div/div/ul/li/div/div[2]/span/input'
+
     account_created = False
 
-    def __init__(self, webdriver):
-        self.webdriver = webdriver
-        webdriver.get("https://konto.onet.pl/register-email.html?app_id=poczta.onet.pl.front")
+    def __init__(self, driver):
+        self.webdriver = driver
+        driver.get("https://konto.onet.pl/register-email.html?app_id=poczta.onet.pl.front")
+        wait = ui.WebDriverWait(driver, 10)
         print("jestesmy w onetemail konstruktor")
 
     def fill_inputs(self, info):
         login = info.getElementsByTagName('login')[0].firstChild.data
-        print("login: " + login)
+        login_node = self.webdriver.find_element_by_id(self.login_selector)
+        login_node.send_keys(login)
+
+        check_login = self.webdriver.find_element_by_xpath(self.check_login_button)
+        check_login.click()
+
+        for text_field in self.inputs:
+            print(text_field + ': ' + self.inputs[text_field])
+            value = info.getElementsByTagName(text_field)[0].firstChild.data
+            node = self.webdriver.find_element_by_id(self.inputs[text_field])
+            node.send_keys(value)
+
+        for dropdown in self.dropdowns_selectors:
+            print(dropdown + ': ' + self.dropdowns_selectors[dropdown])
+            value = info.getElementsByTagName(dropdown)[0].firstChild.data
+            node = self.webdriver.find_element_by_id(self.inputs[text_field])
+            Select(node).select_by_value(value)
+
+
 
     def solve_captcha(self):
 
